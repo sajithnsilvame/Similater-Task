2ae896542717ab23a0f3f98eb4dff20ca1d8d6a1
sajithnsilvame <sajithnsilva.me@gmail.com>
2024-09-01 12:49:45 +0530

    use .env to keep API base url

--- src/pages/PropertyList.tsx
+++ src/pages/PropertyList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import BedroomImg from "../assets/image 4.png";
 import NightImg from "../assets/image 5.png";
@@ -24,7 +24,17 @@ interface Property {
   website_image: string;
 }
 
-const PropertyCard: React.FC<{ property: Property }> = ({ property }) => {
+// Helper function to calculate the number of nights
+const calculateNights = (checkIn: string, checkOut: string): number => {
+  const checkInDate = new Date(checkIn);
+  const checkOutDate = new Date(checkOut);
+  const diffTime = Math.abs(checkOutDate.getTime() - checkInDate.getTime());
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+  return diffDays;
+};
+
+const PropertyCard: FC<{ property: Property }> = ({ property }) => {
+  const nights = calculateNights(property.check_in, property.check_out);
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden mb-4">
       <div className="flex flex-col md:flex-row">
@@ -43,66 +53,105 @@ const PropertyCard: React.FC<{ property: Property }> = ({ property }) => {
         <div className="w-full md:w-3/4 p-4">
           <div className="flex flex-col md:flex-row justify-between items-start mb-2">
             <div>
-              <h2 className="text-xl font-bold">{property.property_name}</h2>
-              <p className="text-sm text-gray-600">
+              <h2 className="text-lg md:text-xl font-bold">
+                {property.property_name}
+              </h2>
+              <p className="text-xs md:text-sm text-gray-600">
                 Property code: {property.property_code}
               </p>
             </div>
-            <p className="text-purple-700 font-bold text-xl mt-2 md:mt-0">
+            <p className="text-purple-700 font-bold text-lg md:text-xl mt-2 md:mt-0">
               USD {Number(property.price).toFixed(2)}
             </p>
           </div>
           <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-4">
-            <p className="text-sm">Check in: {property.check_in}</p>
-            <p className="text-sm my-2 md:my-0">
-              Check out: {property.check_out}
+            <p className="text-xs md:text-sm">
+              Check in: <span className="font-bold">{property.check_in}</span>
             </p>
-            <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded w-full md:w-auto">
+            <p className="text-xs md:text-sm my-2 md:my-0">
+              Check out: <span className="font-bold">{property.check_out}</span>
+            </p>
+            <button className="bg-[#F36F27] hover:bg-[#db682b] text-white font-bold py-2 px-4 rounded-lg w-full md:w-auto">
               Select
             </button>
           </div>
+
           <hr className="border-t border-gray-200 mb-4" />
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
-            <div className="flex items-center">
-              <img src={BedroomImg} alt="Bedrooms" className="h-6 w-6 mr-2" />
+          <div className="grid grid-cols-2 gap-y-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+            <div className="flex flex-col items-center md:flex-row">
+              <img
+                src={BedroomImg}
+                alt="Bedrooms"
+                className="h-5 w-5 md:h-6 md:w-6 mb-1 md:mb-0 md:mr-2"
+              />
               <div>
-                <p className="text-xs">Bedrooms</p>
-                <p className="font-bold">{property.bedrooms}</p>
+                <p className="text-xs text-center md:text-left">Bedrooms</p>
+                <p className="font-bold text-center md:text-left">
+                  {property.bedrooms}
+                </p>
               </div>
             </div>
-            <div className="flex items-center">
-              <img src={NightImg} alt="Nights" className="h-6 w-6 mr-2" />
+            <div className="flex flex-col items-center md:flex-row">
+              <img
+                src={NightImg}
+                alt="Nights"
+                className="h-5 w-5 md:h-6 md:w-6 mb-1 md:mb-0 md:mr-2"
+              />
               <div>
-                <p className="text-xs">Nights</p>
-                <p className="font-bold">{property.bedrooms}</p>
+                <p className="text-xs text-center md:text-left">Nights</p>
+                <p className="font-bold text-center md:text-left">{nights}</p>
               </div>
             </div>
-            <div className="flex items-center">
-              <img src={AdultsImg} alt="Adults" className="h-6 w-6 mr-2" />
+            <div className="flex flex-col items-center md:flex-row">
+              <img
+                src={AdultsImg}
+                alt="Adults"
+                className="h-5 w-5 md:h-6 md:w-6 mb-1 md:mb-0 md:mr-2"
+              />
               <div>
-                <p className="text-xs">Adults</p>
-                <p className="font-bold">{property.adults}</p>
+                <p className="text-xs text-center md:text-left">Adults</p>
+                <p className="font-bold text-center md:text-left">
+                  {property.adults}
+                </p>
               </div>
             </div>
-            <div className="flex items-center">
-              <img src={ChildrenImg} alt="Children" className="h-6 w-6 mr-2" />
+            <div className="flex flex-col items-center md:flex-row">
+              <img
+                src={ChildrenImg}
+                alt="Children"
+                className="h-5 w-5 md:h-6 md:w-6 mb-1 md:mb-0 md:mr-2"
+              />
               <div>
-                <p className="text-xs">Children</p>
-                <p className="font-bold">{property.children}</p>
+                <p className="text-xs text-center md:text-left">Children</p>
+                <p className="font-bold text-center md:text-left">
+                  {property.children}
+                </p>
               </div>
             </div>
-            <div className="flex items-center">
-              <img src={ParkingImg} alt="Parking" className="h-6 w-6 mr-2" />
+            <div className="flex flex-col items-center md:flex-row">
+              <img
+                src={ParkingImg}
+                alt="Parking"
+                className="h-5 w-5 md:h-6 md:w-6 mb-1 md:mb-0 md:mr-2"
+              />
               <div>
-                <p className="text-xs">Parking</p>
-                <p className="font-bold">{property.parking}</p>
+                <p className="text-xs text-center md:text-left">Parking</p>
+                <p className="font-bold text-center md:text-left">
+                  {property.parking}
+                </p>
               </div>
             </div>
-            <div className="flex items-center">
-              <img src={PetImg} alt="Pets" className="h-6 w-6 mr-2" />
+            <div className="flex flex-col items-center md:flex-row">
+              <img
+                src={PetImg}
+                alt="Pets"
+                className="h-5 w-5 md:h-6 md:w-6 mb-1 md:mb-0 md:mr-2"
+              />
               <div>
-                <p className="text-xs">Pets</p>
-                <p className="font-bold">{property.pets}</p>
+                <p className="text-xs text-center md:text-left">Pets</p>
+                <p className="font-bold text-center md:text-left">
+                  {property.pets}
+                </p>
               </div>
             </div>
           </div>
@@ -112,7 +161,9 @@ const PropertyCard: React.FC<{ property: Property }> = ({ property }) => {
   );
 };
 
-const PropertyList: React.FC = () => {
+
+
+const PropertyList: FC = () => {
   const [properties, setProperties] = useState<Property[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -127,8 +178,9 @@ const PropertyList: React.FC = () => {
       }
 
       try {
+        // Here I fetch the properties from the server using fetch() If need we can use Axios
         const response = await fetch(
-          "https://skill-test.similater.website/api/v1/property/list",
+          `${import.meta.env.VITE_API_BASE_URL as string}/api/v1/property/list`,
           {
             headers: {
               Authorization: `Bearer ${accessToken}`,
